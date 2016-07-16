88c386493022c49516ff8fab72311ddaa0e4160b
darui00kara <Takeshi00Miyamoto@gmail.com>
2016-07-16 14:03:48 +0900

    Add TaskController, TaskView, Templates (task)

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Suigin.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/tasks", TaskController
   end
 
   # Other scopes may use custom stacks.
