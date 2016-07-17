3508d40ce7dec062683b6b373438dcc2e93dacc1
hayabusa333 <junnichi0121@gmail.com>
2016-07-17 12:26:44 +0900

    required items add index page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Suigin.PageController do
   use Suigin.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    required_tasks = Suigin.Task |> Repo.all()
+    render conn, "index.html", required_tasks: required_tasks
   end
 end
