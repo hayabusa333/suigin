72fbbe7cd92d691139d21af8b074b88bc50ed1bb
hayabusa333 <junnichi0121@gmail.com>
2016-07-17 11:59:24 +0900

    add task validate

--- web/models/task.ex
+++ web/models/task.ex
@@ -15,6 +15,9 @@ defmodule Suigin.Task do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :status, :category])
+    |> validate_length(:name, min: 1, max: 256)
+    |> validate_number(:status, greater_than_or_equal_to: 1, less_than_or_equal_to: 3)
+    |> validate_number(:category, greater_than_or_equal_to: 1, less_than_or_equal_to: 4)
     |> validate_required([:name, :status, :category])
   end
 
